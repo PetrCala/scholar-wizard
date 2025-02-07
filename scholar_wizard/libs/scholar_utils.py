3cefc11e82582acfd3616a2531557f0c06b51f8e
PetrCala <cala.p@seznam.cz>
2025-02-07 15:08:01 +0100

    Default back to free proxies

--- scholar_wizard/libs/scholar_utils.py
+++ scholar_wizard/libs/scholar_utils.py
@@ -6,6 +6,6 @@ def setup_proxy() -> None:
     """Use a proxy generator to avoid Google Scholar blocking."""
     logger.debug("Using a proxy generator to avoid Google Scholar blocking...")
     pg = ProxyGenerator()
-    pg.Tor_Internal(tor_cmd="/Applications/Tor Browser.app/Contents/MacOS/Tor/tor")
-    # pg.FreeProxies()
+    # pg.Tor_Internal(tor_cmd="/Applications/Tor Browser.app/Contents/MacOS/Tor/tor")
+    pg.FreeProxies()
     scholarly.use_proxy(pg)
